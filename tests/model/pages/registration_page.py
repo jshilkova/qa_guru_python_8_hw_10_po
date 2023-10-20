c3c6e0f9ef797b7f5fa8b7828dcafc29c58932e0
Julia Shilkova <yulia.shikova@gmail.com>
2023-10-21 00:15:22 +0700

    hw 10 part 1 - fluent page object

--- tests/model/pages/registration_page.py
+++ tests/model/pages/registration_page.py
@@ -6,56 +6,67 @@ import tests
 
 
 class RegistrationPage:
-    def __init__(self):
-        pass
-
     def open(self):
         browser.open('/automation-practice-form')
+        return self
 
     def fill_first_name(self, value):
         browser.element('#firstName').type(value)
+        return self
 
     def fill_last_name(self, value):
         browser.element('#lastName').type(value)
+        return self
 
     def fill_email(self, value):
         browser.element('#userEmail').type(value)
+        return self
 
     def select_gender(self, value):
         browser.all('[name="gender"]').element_by(have.value(value)).element('..').click()
+        return self
 
     def fill_mobile(self, value):
         browser.element('#userNumber').type(value)
+        return self
 
     def fill_date_of_birth(self, year, month, day):
         browser.element('#dateOfBirthInput').click()
         browser.element('.react-datepicker__year-select').type(year)
         browser.element('.react-datepicker__month-select').type(month)
         (browser.element(f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)').click())
+        return self
 
     def fill_subjects(self, value):
         browser.element('#subjectsInput').type(value).press_enter()
+        return self
 
     def select_hobbies(self, value):
         browser.all('label[for^="hobbies-checkbox"]').element_by(have.exact_text(value)).click()
+        return self
 
     def upload_picture(self, value):
         (browser.element('#uploadPicture')
          .type(str(Path(tests.__file__).parent.joinpath(value).absolute())))
+        return self
 
     def type_current_address(self, value):
         browser.element('#currentAddress').type(value)
+        return self
 
     def select_state(self, value):
         browser.element('#state').click()
         browser.all('[id^="react-select-3-option"]').element_by(have.exact_text(value)).click()
+        return self
 
     def select_city(self, value):
         browser.element('#city').click()
         browser.all('[id^="react-select-4-option"]').element_by(have.exact_text(value)).click()
+        return self
 
     def submit(self):
         browser.element('#submit').click()
+        return self
 
     def should_have_registered_user(self, full_name, email, gender, mobile, birthday, subjects, hobbies, picture,
                                     address, state_and_city):
--- tests/test_demoqa_automation_practice_form.py
+++ tests/test_demoqa_automation_practice_form.py
@@ -5,19 +5,21 @@ def test_complete_form():
     registration_page = RegistrationPage()
     registration_page.open()
 
-    registration_page.fill_first_name('Iuliia')
-    registration_page.fill_last_name('Shilkova')
-    registration_page.fill_email('yulia.shilkova@gmail.com')
-    registration_page.select_gender('Female')
-    registration_page.fill_mobile('1234567890')
-    registration_page.fill_date_of_birth('1988', 'July', '30')
-    registration_page.fill_subjects('Arts')
-    registration_page.select_hobbies('Sports')
-    registration_page.upload_picture('images/a_nice_cat_image.jpeg')
-    registration_page.type_current_address('My current address')
-    registration_page.select_state('Haryana')
-    registration_page.select_city('Panipat')
-    registration_page.submit()
+    (registration_page
+     .fill_first_name('Iuliia')
+     .fill_last_name('Shilkova')
+     .fill_email('yulia.shilkova@gmail.com')
+     .select_gender('Female')
+     .fill_mobile('1234567890')
+     .fill_date_of_birth('1988', 'July', '30')
+     .fill_subjects('Arts')
+     .select_hobbies('Sports')
+     .upload_picture('images/a_nice_cat_image.jpeg')
+     .type_current_address('My current address')
+     .select_state('Haryana')
+     .select_city('Panipat')
+     .submit())
+
     registration_page.should_have_registered_user(
             'Iuliia Shilkova',
             'yulia.shilkova@gmail.com',
