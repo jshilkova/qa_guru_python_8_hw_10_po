1d9be76b57b6f7b320cdf5ebce01ef9f897532db
Julia Shilkova <yulia.shikova@gmail.com>
2023-11-06 17:51:12 +0700

    add browser zoom instead of increasing ads timeout

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -9,7 +9,7 @@ from demoqa_tests.data.users import User
 class RegistrationPage:
     def open(self):
         browser.open('/automation-practice-form')
-        browser.all('[id^=google_ads][id$=container__]').with_(timeout=100).wait_until(
+        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
             have.size_greater_than_or_equal(3)
         )
         browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
--- tests/conftest.py
+++ tests/conftest.py
@@ -34,6 +34,8 @@ def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
+    driver.execute_script("document.body.style.zoom='80%'")
+
     yield
 
     attach.add_html(browser)
