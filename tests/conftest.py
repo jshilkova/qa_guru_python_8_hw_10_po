4a27c3da6f0bf8fb935f589eb7af27ecdfeba7eb
Julia Shilkova <yulia.shikova@gmail.com>
2023-11-06 18:09:43 +0700

    add dotenv lib to requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,7 +34,6 @@ def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
-    driver.execute_script("document.body.style.zoom='80%'")
 
     yield
 
